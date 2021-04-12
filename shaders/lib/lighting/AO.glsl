5c8319854af660ddf4295f37a2d6e961bb48f927
Eldeston <fremfredric@gmail.com>
2021-04-12 20:11:55 +0400

    -Adjusted AO

--- shaders/lib/lighting/AO.glsl
+++ shaders/lib/lighting/AO.glsl
@@ -1,3 +1,3 @@
 float getAmbient(matPBR material, positionVectors posVec){
-	return max(max((abs(material.normal_m.x) * 0.25 + material.normal_m.y * 0.25 + 0.75) * smootherstep(material.light_m.y), 0.5), material.light_m.x);
+	return max(max((abs(material.normal_m.x) * 0.25 + material.normal_m.y * 0.25 + 0.75) * smootherstep(material.light_m.y), 0.75), material.light_m.x);
 }
