221732d6e0f3c6ac38b78ea295f22152a673e354
Eldeston <fremfredric@gmail.com>
2021-04-13 21:48:17 +0400

    -Adjusted materials for roughness

--- shaders/main/gbuffers_water.glsl
+++ shaders/main/gbuffers_water.glsl
@@ -58,6 +58,7 @@ INOUT mat3 TBN;
         float ss = (entity.x >= 10001 && entity.x <= 10004) || entity.x == 10007 || entity.x == 10011 || entity.x == 10013 ? sqrt(maxCol) * 0.8 : 0.0;
         float emissive = entity.x == 10005 || entity.x == 10006 ? maxCol
             : entity.x == 10014 ? satCol : 0.0;
+        float roughness = (entity.x >= 10008 && entity.x <= 10010) || entity.x == 10015 ? 0.2 * maxCol : 1.0;
 
         vec4 nGlcolor = glcolor * (1.0 - emissive) + sqrt(sqrt(glcolor)) * emissive;
 
@@ -81,7 +82,7 @@ INOUT mat3 TBN;
         gl_FragData[0] = color; //gcolor
         gl_FragData[1] = vec4(normal * 0.5 + 0.5, 1); //colortex1
         gl_FragData[2] = vec4(nLmCoord, ss, 1); //colortex2
-        gl_FragData[3] = vec4(metallic, emissive, 0, 1); //colortex3
+        gl_FragData[3] = vec4(metallic, emissive, roughness, 1); //colortex3
         gl_FragData[4] = vec4(1, 0, color.a, 1); //colortex4
     }
 #endif
