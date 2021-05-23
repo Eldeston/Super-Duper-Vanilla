792e64fb51a45f1f914746473b48d6c79a5e7435
Eldeston <fremfredric@gmail.com>
2021-05-23 13:16:12 +0400

    -Adjusted sky colors

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -7,7 +7,7 @@
 #define RENDER_FOLIAGE_SHD // Enable foliage shadow render
 
 // Off by default
-// #define WHITE_MODE // Enable white mode, reveals ao
+// #define WHITE_MODE // Enable white mode/textureless mode, keeps materials
 // #define WHITE_MODE_F // Enable white mode with foliage colors (if WHITE_MODE is on)
 
 #define BLOOM // Enable bloom
@@ -34,7 +34,7 @@
 
 #define SATURATION 1.4 // Saturation [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0]
 #define EXPOSURE 1.0 // Exposure [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0]
-#define CONTRAST 1.1 // Contrast [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0]
+#define CONTRAST 1.2 // Contrast [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0]
 #define GAMMA 1.0 // Gamma [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0]
 
 #define WATER_BLUR_SIZE 8.0 // Water blur size, the smaller the sharper the waves look, the larger the smoother the waves look [1.0 2.0 4.0 8.0 12.0 16.0 32.0]
@@ -43,9 +43,8 @@
 #define INVERSE // Inverses the heightmap of the water normals which is more realistic
 
 #define SHADOW_FILTER // Enable soft shadow filtering, if enabled shadows will appear softer, this costs performance
-#define SHD_COL
+#define SHD_COL // Enable shadow color from colored transparent objects
 
-// 
 #define SSGI // Enables SSGI, currently experimental and may not be very optimized, may improve the ambience of dark areas despite the noisiness
 #define SSGI_STEPS 16 // SSGI steps, more steps means more quality, and more quality means more performance [16 20 24 28 32]
 #define SSR // Enables SSR, may not look good in certain areas
@@ -60,11 +59,11 @@
 /*---------------------------------------------------------------------------------------------------------------------------*/
 
 #define LIGHT_COL_DAY vec3(2.0, 1.9, 1.8)
-#define LIGHT_COL_NIGHT vec3(0.125, 0.25, 0.5)
+#define LIGHT_COL_NIGHT vec3(0.25, 0.375, 0.5)
 #define LIGHT_COL_DAWN_DUSK vec3(1.2, 0.9, 0.3)
 
 #define SKY_COL_DAY vec3(0.6, 0.8, 1)
 #define SKY_COL_NIGHT vec3(0.0125, 0.025, 0.1)
-#define SKY_COL_DAWN_DUSK vec3(0.24, 0.08, 0.16)
+#define SKY_COL_DAWN_DUSK vec3(0.24, 0.08, 0.24)
 
 #define BLOCK_LIGHT_COL vec3(1, 0.9, 0.8)
