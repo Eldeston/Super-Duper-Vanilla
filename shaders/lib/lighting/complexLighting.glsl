a9f620834cbb7e92ce105fe05ae915f041c4239d
Eldeston <fremfredric@gmail.com>
2021-05-28 21:02:48 +0400

    -Adjusted lighting

--- shaders/lib/lighting/complexLighting.glsl
+++ shaders/lib/lighting/complexLighting.glsl
@@ -16,7 +16,7 @@ vec3 complexLighting(matPBR material, positionVectors posVector, vec3 dither){
 	// Get direct light diffuse color
 	vec3 diffuseCol = getShdMapping(posVector.shdPos, material.normal_m, nLightPos, dither.r, material.ss_m) * lightCol;
 	// Get globally illuminated sky
-	vec3 GISky = getSkyRender(material.normal_m, 0.0, skyCol, lightCol) * material.light_m.y * material.light_m.y;
+	vec3 GISky = getSkyRender(material.normal_m, skyCol, lightCol, 0.0, 0.0) * material.light_m.y * material.light_m.y;
 
 	#ifdef SSGI
 		// Get SSGI
@@ -40,10 +40,11 @@ vec3 complexLighting(matPBR material, positionVectors posVector, vec3 dither){
 	#endif
 	
 	// Get reflected sky
-    vec3 reflectedSkyRender = getSkyRender(reflectedEyePlayerPos, pow(material.light_m.y, 1.0 / 4.0) * sqrtSmoothness, skyCol, lightCol) * material.light_m.y;
+	float skyMask = pow(material.light_m.y, 1.0 / 4.0) * sqrtSmoothness;
+    vec3 reflectedSkyRender = getSkyRender(reflectedEyePlayerPos, skyCol, lightCol, skyMask, skyMask) * material.light_m.y;
 
 	// Mask reflections
-    vec3 reflectCol = mix(reflectedSkyRender, SSRCol.rgb, SSRCol.a) * fresnel * smoothness; // Will change this later...
+    vec3 reflectCol = mix(reflectedSkyRender, SSRCol.rgb, SSRCol.a) * fresnel * sqrtSmoothness; // Will change this later...
 
 	/* Calculate total lighting and return color */
     return material.albedo_t * ((diffuseCol + GISky * material.ambient_m + GIcol + cubed(material.light_m.x) * BLOCK_LIGHT_COL * pow(material.ambient_m, 1.0 / 4.0)) * (1.0 - material.metallic_m) + material.emissive_m) + specCol + reflectCol;
