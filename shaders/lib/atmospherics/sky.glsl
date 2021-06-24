a112271c3e6c08dc72fd1e4040fda79e10a63b7c
Eldeston <fremfredric@gmail.com>
2021-06-24 13:17:58 +0400

    -Adjusted nether and end fog colors

--- shaders/lib/atmospherics/sky.glsl
+++ shaders/lib/atmospherics/sky.glsl
@@ -17,7 +17,7 @@ vec3 getSkyRender(vec3 playerPos, vec3 skyCol, vec3 lightCol, float skyMask, flo
         #ifdef END
             return skyCol;
         #else
-            return sqrt(fogColor);
+            return sqrt(fogColor) * 0.75;
         #endif
     #else
         // Get positions
