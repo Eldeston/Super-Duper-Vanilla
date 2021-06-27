c480117b72b6d885b490ff8c2a357f5c0d9b9a60
Eldeston <fremfredric@gmail.com>
2021-06-27 23:47:58 +0400

    -Made corals wave

--- shaders/lib/vertex/vertexWave.glsl
+++ shaders/lib/vertex/vertexWave.glsl
@@ -4,26 +4,32 @@ void getWave(inout vec3 vertexPos, in vec3 worldPos, in vec2 texCoord, in vec2 m
     float waterWeight = 0.072;
 
 	float globalTime = ANIMATION_SPEED * frameTimeCounter;
-	
-	if((id == 10002 || id == 10003) || id == 10004)
-		plantWeight *= float(texCoord.y < midTexCoord.y) + float(id == 10003);
+	float offSet = float(texCoord.y < midTexCoord.y) + float(id == 10002);
 
-    float windDisp = sin(worldPos.x + worldPos.z * 2.0 + globalTime * 1.36) * plantWeight;
+	if((id >= 10000 && id <= 10003) || id == 10008){
+		plantWeight *= offSet;
+		waterWeight *= offSet;
+	}
+
+    float windDisp = sin(worldPos.x + worldPos.z * 2.0 + globalTime * 1.36) * plantWeight * outSide;
 	float waterDisp = sin(worldPos.x + worldPos.z + globalTime * 1.6) * waterWeight;
 	
 	// Tall grass
-	if((id >= 10001 && id <= 10004) || id == 10007)
+	if((id >= 10000 && id <= 10003) || id == 10006)
         vertexPos.x += windDisp;
 	// Foliage
-	else if(id == 10006)
+	else if(id == 10005)
 		vertexPos.xz += windDisp * 0.75;
 	// Water and lava
     else if(id == 10010 || id == 10014)
 		vertexPos.y += waterDisp;
+	// Corals
+	else if(id == 10008)
+		vertexPos.x += waterDisp * 2.0;
 	// Vines
-    else if(id == 10008)
+    else if(id == 10007)
 		vertexPos.x += windDisp * 0.32;
 	// Floating plants
-	else if(id == 10005)
+	else if(id == 10004)
 		vertexPos.y += waterDisp;
 }
