791dae3327f9cca6496b9695069224ddd231ee8a
Eldeston <fremfredric@gmail.com>
2021-06-27 18:25:22 +0400

    -Adjusted volumetric lighting

--- shaders/lib/rayTracing/volLight.glsl
+++ shaders/lib/rayTracing/volLight.glsl
@@ -4,6 +4,7 @@ vec3 getGodRays(vec3 feetPlayerPos, float worldPosY, float dither){
 	#else
 		if(VOL_LIGHT_BRIGHTNESS == 0) return vec3(0);
 
+		float underWaterMult = float(isEyeInWater + 1);
 		feetPlayerPos *= 1.0 + dither * 0.3333;
 
 		vec3 rayData = vec3(0);
@@ -14,9 +15,9 @@ vec3 getGodRays(vec3 feetPlayerPos, float worldPosY, float dither){
 			shdPos = distort(shdPos) * 0.5 + 0.5;
 
 			vec3 rayCol = getShdTex(shdPos);
-			float fog = atmoFog(feetPlayerPos.y, worldPosY, length(feetPlayerPos), 0.08, 0.07);
+			float fog = atmoFog(feetPlayerPos.y, worldPosY, length(feetPlayerPos), 0.08 * underWaterMult, 0.07 * underWaterMult);
 			rayData = mix(rayData, rayCol, fog);
 		}
-		return rayData * float(isEyeInWater + 1) * VOL_LIGHT_BRIGHTNESS * (1.0 - newTwilight) * (1.0 - blindness * 0.5);
+		return rayData * VOL_LIGHT_BRIGHTNESS * (1.0 - newTwilight) * (1.0 - blindness * 0.5);
 	#endif
 }
