8a6ab707110e85bd75b944f9d86c5ebfd42e9524
Eldeston <fremfredric@gmail.com>
2021-07-11 01:21:45 +0400

    -Adjusted sky colors

--- shaders/lib/atmospherics/sky.glsl
+++ shaders/lib/atmospherics/sky.glsl
@@ -36,7 +36,7 @@ vec3 getSkyRender(vec3 playerPos, vec3 skyCol, vec3 lightCol, float skyMask, flo
 
         // Get star
         vec2 starPos = 0.5 > abs(nSkyPos.y) ? vec2(atan(nSkyPos.x, nSkyPos.z), nSkyPos.y) * 0.25 : nSkyPos.xz * 0.333;
-        float star = genStar(starPos * 0.128) * cubed(1.0 - day);
+        float star = genStar(starPos * 0.128) * squared(1.0 - day);
 
         vec3 celestialBodies = (star + sunMoon * dirLightMask * 8.0 * sqrt(lightCol)) * skyMask * voidGradient;
         
