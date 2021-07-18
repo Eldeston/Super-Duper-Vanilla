cb468cb40b89673ee5f1b74cc80c6bc1ccbd7de5
Eldeston <fremfredric@gmail.com>
2021-07-18 13:51:05 +0400

    -Added luminance based bloom in settings

--- shaders/main/composite1.glsl
+++ shaders/main/composite1.glsl
@@ -15,11 +15,13 @@ INOUT vec2 texcoord;
     const bool colortex4MipmapEnabled = true;
 
     uniform sampler2D gcolor;
-    uniform sampler2D colortex3;
     uniform sampler2D colortex4;
 
+    #if BLOOM == 1
+        uniform sampler2D colortex3;
+    #endif
+
     #include "/lib/globalVars/gameUniforms.glsl"
-    #include "/lib/globalVars/screenUniforms.glsl"
     #include "/lib/globalVars/timeUniforms.glsl"
     #include "/lib/globalVars/universalVars.glsl"
 
@@ -28,13 +30,22 @@ INOUT vec2 texcoord;
 
     void main(){
         vec3 sceneCol = texture2D(gcolor, texcoord).rgb;
-        vec3 color = sceneCol * texture2D(colortex3, texcoord).g;
+
+        #if BLOOM == 1
+            vec3 bloomCol = sceneCol * texture2D(colortex3, texcoord).g;
+        #elif BLOOM == 2
+            vec3 bloomCol = sceneCol * smoothstep(1.0, 4.0, getLuminance(sceneCol));
+        #endif
 
         float volMult = VOL_LIGHT_BRIGHTNESS * (1.0 - newTwilight) * (1.0 - blindness * 0.6) * (0.25 * (1.0 - eyeBrightFact) + eyeBrightFact) * min(1.0, FOG_OPACITY + rainMult * underWaterMult * 0.16);
 
-    /* DRAWBUFFERS:02 */
+    /* DRAWBUFFERS:0 */
         gl_FragData[0] = vec4(sceneCol + (texture2D(colortex4, texcoord, 1.6).gba * volMult) * lightCol, 1); //gcolor
-        // Compress the HDR colors
-        gl_FragData[1] = vec4(color / (1.0 + color), 1); //colortex2
+
+        #if BLOOM != 0
+        /* DRAWBUFFERS:02 */
+            // Compress the HDR colors
+            gl_FragData[1] = vec4(bloomCol / (1.0 + bloomCol), 1); //colortex2
+        #endif
     }
 #endif
