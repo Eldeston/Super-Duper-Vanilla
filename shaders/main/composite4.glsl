3df1b3b2f440b1a4d50e0ab176f64cfa20ed1630
Eldeston <fremfredric@gmail.com>
2021-07-30 22:07:41 +0400

    -Fixed temporal accumulation error

--- shaders/main/composite4.glsl
+++ shaders/main/composite4.glsl
@@ -34,6 +34,9 @@ INOUT vec2 texcoord;
         uniform sampler2D depthtex0;
 
         // Previous camera position
+        uniform vec3 cameraPosition;
+
+        // Previous camera position
         uniform vec3 previousCameraPosition;
 
         // View matrix uniforms
@@ -55,7 +58,18 @@ INOUT vec2 texcoord;
 
     #ifdef TEMPORAL_ACCUMULATION
         #include "/lib/lighting/shdDistort.glsl"
-        #include "/lib/utility/spaceConvert.glsl"
+
+        vec2 toPrevScreenPos(vec2 currentPos){
+            // Previous frame reprojection from Chocapic13
+            vec4 viewPosPrev = gbufferProjectionInverse * vec4(vec3(currentPos.xy, texture2D(depthtex0, currentPos.xy).x) * 2.0 - 1.0, 1);
+            viewPosPrev /= viewPosPrev.w;
+            viewPosPrev = gbufferModelViewInverse * viewPosPrev;
+
+            vec4 prevPosition = viewPosPrev + vec4(cameraPosition - previousCameraPosition, 0);
+            prevPosition = gbufferPreviousModelView * prevPosition;
+            prevPosition = gbufferPreviousProjection * prevPosition;
+            return prevPosition.xy / prevPosition.w * 0.5 + 0.5;
+        }
     #endif
 
     #include "/lib/post/spectral.glsl"
