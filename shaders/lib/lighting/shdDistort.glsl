c0e382630b7f38fc342c051b27df2dcd127f6b7e
Eldeston <fremfredric@gmail.com>
2021-10-07 15:27:39 +0400

    -Adjusted shadow distort

--- shaders/lib/lighting/shdDistort.glsl
+++ shaders/lib/lighting/shdDistort.glsl
@@ -1,13 +1,6 @@
 #ifdef SHADOW_DISTORT
-	//euclidian distance is defined as sqrt(a^2 + b^2 + ...)
-	//this length function instead does cbrt(a^3 + b^3 + ...)
-	//this results in smaller distances along the diagonal axes.
-	float cubeLength(vec2 v){
-		return pow(abs(v.x * v.x * v.x) + abs(v.y * v.y * v.y), 1.0 / 3.0);
-	}
-	
 	float getDistortFactor(vec2 v){
-		return cubeLength(v) + SHADOW_DISTORT_FACTOR;
+		return length(v) + SHADOW_DISTORT_FACTOR;
 	}
 #else
 	float getDistortFactor(vec2 v){
