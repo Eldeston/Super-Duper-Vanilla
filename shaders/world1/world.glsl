54ffbfddb6d3caae45511a6c837a75278bab5f75
Eldeston <fremfredric@gmail.com>
2021-11-26 01:00:08 +0400

    -Adjusted end sky

--- shaders/world1/world.glsl
+++ shaders/world1/world.glsl
@@ -15,12 +15,12 @@ allowing more compatibility for future worlds/dimensions and modded worlds/dimen
 // Enable stars in your world
 #define USE_STARS_COL vec3(2, 1, 4)
 // Enable horizon in your world
-#define USE_HORIZON_COL vec3(0.08, 0.04, 0.24)
+#define USE_HORIZON_COL vec3(0.1125, 0.1, 0.2)
 
 // Enable if your world uses a specific world color that uses the vanilla fog color, overrides sky colors
 // #define USE_VANILLA_FOGCOL fogColor
 // Enable if your world uses a custom fog color with an input, overrides sky colors
-#define USE_CUSTOM_FOGCOL vec3(0.1125, 0.1, 0.2)
+#define USE_CUSTOM_FOGCOL vec3(0.0625, 0.05, 0.1)
 
 // Disable if your world has an undefined lighting environment like The End or the Nether
 // #define USE_SKY_LIGHTMAP
@@ -32,4 +32,4 @@ allowing more compatibility for future worlds/dimensions and modded worlds/dimen
 // Total density falloff, larger means thicker fog
 #define FOG_TOTAL_DENSITY_FALLOFF 0.01
 // Fog opacity
-#define FOG_OPACITY 0.5
+#define FOG_OPACITY 0.4
