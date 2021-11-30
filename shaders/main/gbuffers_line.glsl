d966eb0c3db15b140df5d707b47abd3c8bc7b39b
Eldeston <fremfredric@gmail.com>
2021-11-30 14:15:47 +0400

    -Fixed invisible fishing line and block outline

--- shaders/main/gbuffers_line.glsl
+++ shaders/main/gbuffers_line.glsl
@@ -5,8 +5,34 @@
 INOUT vec4 glcolor;
 
 #ifdef VERTEX
-    void main(){
-        gl_Position = ftransform();
+    /* Screen resolutions */
+    uniform float viewWidth;
+    uniform float viewHeight;
+
+    const float VIEW_SHRINK = 1.0 - (1.0 / 256.0);
+    const mat4 VIEW_SCALE = mat4(
+        VIEW_SHRINK, 0.0, 0.0, 0.0,
+        0.0, VIEW_SHRINK, 0.0, 0.0,
+        0.0, 0.0, VIEW_SHRINK, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+
+    void main() {
+        vec4 linePosStart = gl_ProjectionMatrix * VIEW_SCALE * gl_ModelViewMatrix * vec4(gl_Vertex.xyz, 1.0);
+        vec4 linePosEnd = gl_ProjectionMatrix * VIEW_SCALE * gl_ModelViewMatrix * vec4(gl_Vertex.xyz + gl_Normal.xyz, 1.0);
+
+        vec3 ndc1 = linePosStart.xyz / linePosStart.w;
+        vec3 ndc2 = linePosEnd.xyz / linePosEnd.w;
+
+        vec2 ScreenSize = vec2(viewWidth, viewHeight);
+        vec2 lineScreenDirection = normalize((ndc2.xy - ndc1.xy) * ScreenSize);
+        vec2 lineOffset = vec2(-lineScreenDirection.y, lineScreenDirection.x) * 1.0 / ScreenSize;
+
+        if(lineOffset.x < 0.0) lineOffset *= -1.0;
+
+        if (gl_VertexID % 2 == 0) gl_Position = vec4((ndc1 + vec3(lineOffset, 0.0)) * linePosStart.w, linePosStart.w);
+        else gl_Position = vec4((ndc1 - vec3(lineOffset, 0.0)) * linePosStart.w, linePosStart.w);
+
         glcolor = gl_Color;
     }
 #endif
