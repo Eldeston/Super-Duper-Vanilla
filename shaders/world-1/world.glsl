53c5ca222c387f8893ed9d618b8164a0b7ef8043
Eldeston <fremfredric@gmail.com>
2021-12-29 17:09:26 +0400

    -Adjusted nether fog settings

--- shaders/world-1/world.glsl
+++ shaders/world-1/world.glsl
@@ -32,8 +32,8 @@ allowing more compatibility for future worlds/dimensions and modded worlds/dimen
 #define SKY_LIGHT_AMOUNT 0.6
 
 // Vertical density falloff, larger means less thick fog at high altitudes, but thicker fog in lower altitudes
-#define FOG_VERTICAL_DENSITY_FALLOFF 0.01
+#define FOG_VERTICAL_DENSITY_FALLOFF 0.025
 // Total density falloff, larger means thicker fog
-#define FOG_TOTAL_DENSITY_FALLOFF 0.01
+#define FOG_TOTAL_DENSITY_FALLOFF 0.1
 // Fog opacity
-#define FOG_OPACITY 0.75
+#define FOG_OPACITY 0.8
