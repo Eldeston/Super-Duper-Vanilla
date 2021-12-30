0e5961b6824fed407ca7d201ea8d259a27c98402
Eldeston <fremfredric@gmail.com>
2021-12-31 02:41:42 +0400

    -Fixed raytracer step size

--- shaders/lib/rayTracing/rayTracer.glsl
+++ shaders/lib/rayTracing/rayTracer.glsl
@@ -1,16 +1,17 @@
 vec3 binarySearch(vec3 screenPosRayDir, vec3 startPos, int binarySearchSteps){
 	for(int y = 0; y < binarySearchSteps; y++){
-		vec3 screenPos = startPos;
-		if(screenPos.x < 0 || screenPos.y < 0 || screenPos.x > 1 || screenPos.y > 1) break;
+		if(startPos.x < 0 || startPos.y < 0 || startPos.x > 1 || startPos.y > 1) break;
 
 		screenPosRayDir *= 0.5;
-		startPos += texture2D(depthtex0, screenPos.xy).x < screenPos.z ? -screenPosRayDir : screenPosRayDir;
+		startPos += texture2D(depthtex0, startPos.xy).x < startPos.z ? -screenPosRayDir : screenPosRayDir;
 	}
 
 	return startPos;
 }
 
 // This raytracer is so fast I swear...
+// Based from Belmu's raytracer https://github.com/BelmuTM/NobleRT
+// (it's basically an upgrade to Shadax's raytracer https://github.com/Shadax-stack/MinecraftSSR)
 vec3 rayTraceScene(vec3 screenPos, vec3 viewPos, vec3 rayDir, int steps, int binarySearchSteps){
 	// If hand, do simple, flipped reflections
 	if((screenPos.z * 2.0 - 1.0) < MC_HAND_DEPTH){
@@ -19,7 +20,7 @@ vec3 rayTraceScene(vec3 screenPos, vec3 viewPos, vec3 rayDir, int steps, int bin
 	}
 
 	// Get screenspace rayDir
-	vec3 screenPosRayDir = normalize(toScreen(viewPos + rayDir) - screenPos) * (2.0 / steps);
+	vec3 screenPosRayDir = normalize(toScreen(viewPos + rayDir) - screenPos) * (1.0 / steps);
 
 	// Screen pos is our startPos
 	for(int x = 0; x < steps; x++){
