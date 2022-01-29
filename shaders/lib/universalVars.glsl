70c0bcf5f89d7eeed94d23d3c0fa2c2f43a241f1
Eldeston <fremfredric@gmail.com>
2022-01-30 02:31:28 +0400

    -Fixed bug with end colors

--- shaders/lib/universalVars.glsl
+++ shaders/lib/universalVars.glsl
@@ -7,7 +7,6 @@
 float torchBrightFact = eyeBrightnessSmooth.x / 240.0;
 
 float newDawnDusk = smoothstep(0.32, 0.96, dawnDusk);
-float newTwilight = cubed(twilight);
 
 float newRainStrength = saturate(rainStrength * eyeBrightFact * float(isEyeInWater != 1));
 float rainMult = newRainStrength + 1.0;
@@ -16,15 +15,15 @@ float underWaterMult = isEyeInWater + 1.0;
 float ambientLighting = pow(AMBIENT_LIGHTING + nightVision * 0.5, GAMMA);
 
 #ifdef WORLD_LIGHT
-    #ifdef USE_CUSTOM_LIGHTCOL
-        vec3 lightCol = pow(USE_CUSTOM_LIGHTCOL, vec3(GAMMA)) * (1.0 - newTwilight);
+    #ifdef WORLD_CUSTOM_LIGHTCOL
+        vec3 lightCol = pow(WORLD_CUSTOM_LIGHTCOL * 0.00392156863, vec3(GAMMA)) * (1.0 - cubed(twilight));
     #else
-        vec3 lightCol = pow(toneSaturation(mix(mix(LIGHT_N, LIGHT_D, day), LIGHT_DD, newDawnDusk) * 0.00392156863, 1.0 - rainStrength * 0.5), vec3(GAMMA)) * (1.0 - newTwilight);
+        vec3 lightCol = pow(toneSaturation(mix(mix(LIGHT_N, LIGHT_D, day), LIGHT_DD, newDawnDusk) * 0.00392156863, 1.0 - rainStrength * 0.5), vec3(GAMMA)) * (1.0 - cubed(twilight));
     #endif
 #endif
 
-#if defined WORLD_CUSTOM_FOGCOL
-    const vec3 skyCol = pow(WORLD_CUSTOM_FOGCOL, vec3(GAMMA));
+#ifdef WORLD_CUSTOM_FOGCOL
+    const vec3 skyCol = pow(WORLD_CUSTOM_FOGCOL * 0.00392156863, vec3(GAMMA));
 #elif defined WORLD_VANILLA_FOGCOL
     vec3 skyCol = pow(WORLD_VANILLA_FOGCOL, vec3(GAMMA));
 #else
