c78f7493cfd9638718bbf28ec66887fc5fe9cf56
Eldeston <fremfredric@gmail.com>
2022-02-08 17:55:37 +0400

    -Removed POM for Default PBR (it didn't look good)

--- shaders/lib/lighting/PBR.glsl
+++ shaders/lib/lighting/PBR.glsl
@@ -155,54 +155,25 @@ uniform sampler2D texture;
         } else return;
     }
 #else
-    #if (defined TERRAIN || defined WATER || defined BLOCK) && defined PARALLAX_OCCLUSION
-        vec2 parallaxUv(sampler2D heightMap, vec2 startUv, vec2 endUv){
-            float currDepth = length(texture2DGradARB(heightMap, fract(startUv) * vTexCoordScale + vTexCoordPos, dcdx, dcdy).rgb);
-            float depth = 1.0;
-
-            const float stepSize = 1.0 / PARALLAX_STEPS;
-            endUv *= stepSize * PARALLAX_DEPTH;
-
-            while(depth >= currDepth){
-                startUv += endUv;
-                currDepth = length(texture2DGradARB(heightMap, fract(startUv) * vTexCoordScale + vTexCoordPos, dcdx, dcdy).rgb);
-                depth -= stepSize;
-            }
-
-            return startUv;
-        }
-    #endif
-
     void getPBR(inout matPBR material, in positionVectors posVector, in int id){
         // Assign default normal map
         material.normal = TBN[2];
 
-        #if (defined TERRAIN || defined WATER || defined BLOCK) && defined PARALLAX_OCCLUSION
-            // Exclude signs, due to a missing text bug
-            vec2 st = id == 10102 ? texCoord : fract(parallaxUv(texture, vTexCoord, viewTBN.xy / -viewTBN.z)) * vTexCoordScale + vTexCoordPos;
-        #else
-            vec2 st = texCoord;
-        #endif
+        vec2 st = texCoord;
 
-        // Generate bumped normals
-        #if (defined TERRAIN || defined WATER || defined BLOCK) && (defined PARALLAX_OCCLUSION || defined AUTO_GEN_NORM)
-            // Assign albedo
-            material.albedo = texture2DGradARB(texture, st, dcdx, dcdy);
+        // Assign albedo
+        material.albedo = texture2DGradARB(texture, st, dcdx, dcdy);
 
-            #ifdef AUTO_GEN_NORM
-                // Don't generate normals if it's on the edge of the texture
+        if(material.albedo.a > 0.00001){
+            // Generate bumped normals
+            #if (defined TERRAIN || defined WATER || defined BLOCK) && defined AUTO_GEN_NORM
                 float d = length(material.albedo.rgb);
                 float dx = d - length(texture2DGradARB(texture, fract(vTexCoord + vec2(0.0125, 0)) * vTexCoordScale + vTexCoordPos, dcdx, dcdy).rgb);
                 float dy = d - length(texture2DGradARB(texture, fract(vTexCoord + vec2(0, 0.0125)) * vTexCoordScale + vTexCoordPos, dcdx, dcdy).rgb);
 
                 material.normal = normalize(TBN * normalize(vec3(dx, dy, 0.125)));
             #endif
-        #else
-            // Assign albedo
-            material.albedo = texture2DGradARB(texture, st, dcdx, dcdy);
-        #endif
 
-        if(material.albedo.a > 0.00001){
             // Default material if not specified
             material.metallic = 0.04; material.emissive = 0.0;
             material.smoothness = 0.0; material.ss = 0.0;
