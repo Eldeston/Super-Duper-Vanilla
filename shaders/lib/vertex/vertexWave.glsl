cf8ccdb90f40915c7e21565c8720e5b6e491b972
Eldeston <fremfredric@gmail.com>
2022-03-04 16:55:25 +0400

    Added more animated objects

--- shaders/lib/vertex/vertexWave.glsl
+++ shaders/lib/vertex/vertexWave.glsl
@@ -7,9 +7,25 @@ void getWave(inout vec3 vertexPos, in vec3 worldPos, in vec2 texCoord, in vec2 m
 	float plantWeight = 0.128; float waterWeight = 0.072;
 
 	// For grounded objects
-	if((id >= 10005 && id <= 10007) || id == 10009 || (id >= 10013 && id <= 10015) || id == 10011 || id == 10036){
-		float offSet = float(texCoord.y < midTexCoord.y) + float(id == 10007 || id == 10015);
+	if((id >= 10005 && id <= 10007) || (id >= 10010 && id <= 10012) || id == 10036){
+		float offSet = float(texCoord.y < midTexCoord.y) + float(id == 10007 || id == 10012);
 		
+		plantWeight *= offSet;
+		waterWeight *= offSet;
+	}
+
+	// For hanged objects
+	if(id == 10015){
+		float offSet = float(texCoord.y > midTexCoord.y);
+		
+		plantWeight *= offSet;
+		waterWeight *= offSet;
+	}
+
+	// Lantern
+	if(id == 10039){
+		float offSet = 1.0 - sqrt(fract(worldPos.y - 0.001));
+
 		plantWeight *= offSet;
 		waterWeight *= offSet;
 	}
@@ -23,16 +39,19 @@ void getWave(inout vec3 vertexPos, in vec3 worldPos, in vec2 texCoord, in vec2 m
 		// Leaves
 		if(id == 10004) vertexPos.xz += windDisp * 0.72;
 
-		// Single and doubles land
-		if((id >= 10005 && id <= 10009) || id == 10033 || id == 10036) vertexPos.x += windDisp;
+		// Single and doubles grounded and hanged land
+		if((id >= 10005 && id <= 10008) || id == 10015 || id == 10033 || id == 10036) vertexPos.x += windDisp;
 		// Sided land
-		if(id == 10010) vertexPos.x += windDisp * 0.32;
+		if(id == 10009) vertexPos.x += windDisp * 0.32;
 
-		// Single and doubles underwater
-		if(id >= 10011 && id <= 10015) vertexPos.x += waterDisp;
+		// Single and doubles grounded and hanged underwater
+		if(id >= 10010 && id <= 10013) vertexPos.x += waterDisp;
 
 		// Floaters
-		if(id == 10016) vertexPos.y += waterDisp;;
+		if(id == 10014) vertexPos.y += waterDisp;;
+
+		// Lanterns
+		if(id == 10039) vertexPos.xz += windDisp * 0.32;
 	#endif
 
 	#if defined WATER || defined SHADOW
