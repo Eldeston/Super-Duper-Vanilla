21747c95ebc4dc31916cccaf071f2b3f6872f3c5
Eldeston <fremfredric@gmail.com>
2022-03-11 17:58:14 +0400

    Adjusted VL vertical density

--- shaders/lib/rayTracing/volLight.glsl
+++ shaders/lib/rayTracing/volLight.glsl
@@ -9,21 +9,21 @@ vec3 getGodRays(vec3 feetPlayerPos, float dither){
 		if(VOL_LIGHT_BRIGHTNESS == 0) return vec3(0);
 
 		float c = WORLD_FOG_TOTAL_DENSITY * (isEyeInWater * 2.56 + newRainStrength + 1.0);
-		float b = WORLD_FOG_VERTICAL_DENSITY * 1.6;
+		float b = WORLD_FOG_VERTICAL_DENSITY * 2.0;
 
 		float nPlayerPosY = normalize(feetPlayerPos).y;
 
 		#ifdef SHD_ENABLE
-			vec3 endPos = feetPlayerPos * 0.125;
+			vec3 endPos = feetPlayerPos * 0.14285714;
 			vec3 startPos = endPos * dither;
 
 			vec3 rayData = vec3(0);
 			for(int x = 0; x < 7; x++){
-				startPos += endPos;
 				rayData = mix(rayData, 
 					getShdTex(distort(mat3(shadowProjection) * (mat3(shadowModelView) * startPos + shadowModelView[3].xyz) + shadowProjection[3].xyz) * 0.5 + 0.5),
 					atmoFog(nPlayerPosY, startPos.y + cameraPosition.y, length(startPos), c, b)
 				);
+				startPos += endPos;
 			}
 			
 			return rayData;
