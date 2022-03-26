397e1b80ab9cec04c51188d4ec236adea1cba850
Eldeston <fremfredric@gmail.com>
2022-03-26 16:34:57 +0400

    SSAO optimizations

--- shaders/lib/lighting/ambientOcclusion.glsl
+++ shaders/lib/lighting/ambientOcclusion.glsl
@@ -3,14 +3,14 @@ float getAmbientOcclusion(vec3 viewPos, vec3 normal, vec3 dither){
 
     for(int i = 0; i < 8; i++){
         // Calculate the offsets
-        vec3 sampleDir = normal + fract(dither + i * PI) - 0.5;
+        vec3 sampleDir = normal + fract(dither + i * 0.125) - 0.5;
         // Add offsets to origin
         vec3 samplePos = viewPos + normalize(sampleDir) * 0.5;
         // Get the sample new depth and linearize
         float sampleDepth = toView(texture2D(depthtex0, toScreen(samplePos).xy).x);
 
         // Check if the offset points are inside geometry or if the point is occluded
-        occlusion += sampleDepth > samplePos.z ? smootherstep(0.5 / abs(viewPos.z - sampleDepth)) : 0.0;
+        occlusion += sampleDepth > samplePos.z ? smoothen(0.5 / abs(viewPos.z - sampleDepth)) : 0.0;
     }
     
     // Invert results and 
