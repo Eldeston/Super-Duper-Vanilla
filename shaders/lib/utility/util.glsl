2c2fede9cc4b7f8ab3527ef9596ed5b5fc0e0124
Eldeston <fremfredric@gmail.com>
2022-04-09 20:38:22 +0400

    Small nerfs

--- shaders/lib/utility/util.glsl
+++ shaders/lib/utility/util.glsl
@@ -1,9 +1,9 @@
 // --------------- Code Library ---------------- //
 // Common utility functions goes here
 
-#define PI 3.141592653589793
-#define PI2 6.283185307179586
-#define PI3 9.42477796076938
+#define PI 3.1415927
+#define PI2 6.2831853
+#define PI3 9.424778
 
 #define ALPHA_THRESHOLD 0.005
 
@@ -108,104 +108,6 @@ vec3 toneContrast(vec3 col, float a){
 mat2 rot2D(float x){
     return mat2(cos(x),-sin(x), sin(x),cos(x));
 }
-
-vec3 rgb2hsv(vec4 c){
-	vec4 K = vec4(0, -1. / 3., 2. / 3., -1);
-	vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
-	vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
-	float d = q.x - min(q.w, q.y);
-	float e = 1e-10;
-	return vec3(abs(q.z + (q.w - q.y) / (6. * d + e)), d / (q.x + e), q.x);
-}
-
-vec3 rgb2hsv(vec3 c){
-	vec4 K = vec4(0, -1. / 3., 2. / 3., -1);
-	vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
-	vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
-	float d = q.x - min(q.w, q.y);
-	float e = 1e-10;
-	return vec3(abs(q.z + (q.w - q.y) / (6. * d + e)), d / (q.x + e), q.x);
-}
-
-// For converting hsv to rgb
-vec3 hsv2rgb(vec3 c){
-	vec4 K = vec4(1, 2. / 3., 1. / 3., 3);
-	vec3 p = abs(fract(c.xxx + K.xyz) * 6. - K.www);
-	return vec3(c.z * mix(K.xxx, clamp(p - K.xxx, 0., 1.), c.y));
-}
-
-float modFract(float x, float tile){
-	return fract(x / tile) * tile;
-}
-
-vec2 modFract(vec2 x, float tile){
-	return fract(x / tile) * tile;
-}
-
-vec2 modFract(vec2 x, vec2 tile){
-	return fract(x / tile) * tile;
-}
-
-vec3 modFract(vec3 x, float tile){
-	return fract(x / tile) * tile;
-}
-
-vec3 modFract(vec3 x, vec3 tile){
-	return fract(x / tile) * tile;
-}
-
-vec4 modFract(vec4 x, float tile){
-	return fract(x / tile) * tile;
-}
-
-vec4 modFract(vec4 x, vec4 tile){
-	return fract(x / tile) * tile;
-}
-
-// Noise functions, all the values are hardcoded to highp, don't change precisions
-// Seeds, (adjust it if you like)
-vec4 s0 = vec4(12.9898, 4.1414, 78.233, 314.13);
-// Must be 1 integer apart ex. 0.36, 1.36, 2.36.....
-vec4 s1 = vec4(0.1031, 1.1031, 2.1031, 3.1031);
-
-// Noise functions
-// 1 out, 1 in...
-float rand11(float n){
-	return fract(sin(dot(n, s0.x)) * 1e4);
-	}
-
-// 1 out, 2 in...
-float rand12(vec2 n){
-	return fract(sin(dot(n, s0.xy)) * 1e4);
-	}
-
-// 2 out, 2 in...
-vec2 rand22(vec2 n){
-	return fract(sin(vec2(dot(n, s0.xy), dot(n, s0.zw))) * 1e4);
-	}
-
-// 3 out, 2 in...
-vec3 rand32(vec2 n){
-	return fract(sin(vec3(dot(n, s0.xy), dot(n, s0.yz), dot(n, s0.zw))) * 1e4);
-	}
-	
-// 3 out, 3 in...
-vec3 rand33(vec3 n){
-	return fract(sin(vec3(dot(n, s0.xyz), dot(n, s0.yzw), dot(n, s0.zwx))) * 1e4);
-	}
-
-// Modified value noise for the beams
-float vnoise(float p){
-	float i = floor(p); float f = fract(p);
-	return mix(rand11(i), rand11(i + 1.0), f * f * f * (f * (f * 6.0 - 15.0) + 10.0));
-	}
-
-float vnoise(vec2 p, float time, float tiles){
-	p = p * tiles + time;
-	vec2 i = floor(p); vec2 f = fract(p);
-	vec2 u = f * f * f * (f * (f * 6.0 - 15.0) + 10.0);
-	return mix(mix(rand12(modFract(i, tiles)), rand12(modFract(i + vec2(1, 0), tiles)), u.x), mix(rand12(modFract(i + vec2(0, 1), tiles)), rand12(modFract(i + 1.0, tiles)), u.x), u.y);
-	}
 
 float edgeVisibility(vec2 screenPos){
     return smoothstep(0.0, 0.025, min2(screenPos * (1.0 - screenPos)));
