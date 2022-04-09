f80622e2f9ea93bbad96da909b2338e9b6b1ef66
Eldeston <fremfredric@gmail.com>
2022-04-09 19:00:10 +0400

    Final lava visual adjustments

--- shaders/lib/surface/lava.glsl
+++ shaders/lib/surface/lava.glsl
@@ -1,20 +1,4 @@
-float getLavaNoiseBilinear(vec2 uv){
-    float pixSize = 1.0 / noiseTextureResolution;
-
-    float downLeft = texture2D(noisetex, uv).x;
-    float downRight = texture2D(noisetex, uv + vec2(pixSize, 0)).x;
-
-    float upRight = texture2D(noisetex, uv + vec2(0, pixSize)).x;
-    float upLeft = texture2D(noisetex, uv + pixSize).x;
-
-    float a = fract(uv.x * noiseTextureResolution);
-
-    float horizontal0 = mix(downLeft, downRight, a);
-    float horizontal1 = mix(upRight, upLeft, a);
-    return mix(horizontal0, horizontal1, fract(uv.y * noiseTextureResolution));
-}
-
 float getLavaNoise(vec2 uv){
-    float animateTime = CURRENT_SPEED * newFrameTimeCounter * 0.03125;
-    return (1.0 - texture2D(noisetex, (uv - animateTime) * 0.5).z) * 0.667 + texture2D(noisetex, uv + animateTime).z * 0.333;
+    float animateTime = CURRENT_SPEED * newFrameTimeCounter * 0.015625;
+    return (1.0 - texture2D(noisetex, uv * 0.5 - animateTime).z) * 0.7 + texture2D(noisetex, (uv + animateTime) * 2.0).z * 0.3;
 }
