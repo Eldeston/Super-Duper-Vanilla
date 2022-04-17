d475be4ce819c643cedb8ebd5fffb2a2c3e92425
Eldeston <fremfredric@gmail.com>
2022-04-17 20:19:53 +0400

    Lava noise are more random

--- shaders/lib/surface/lava.glsl
+++ shaders/lib/surface/lava.glsl
@@ -1,4 +1,4 @@
 float getLavaNoise(vec2 uv){
     float animateTime = CURRENT_SPEED * newFrameTimeCounter * 0.015625;
-    return (1.0 - texture2D(noisetex, uv * 0.5 - animateTime).z) * 0.7 + texture2D(noisetex, (uv + animateTime) * 2.0).z * 0.3;
+    return (1.0 - texture2D(noisetex, uv * 0.5 - animateTime).z) * 0.7 + texture2D(noisetex, (animateTime - uv) * 2.0).z * 0.3;
 }
