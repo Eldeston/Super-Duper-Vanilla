e7e0eb97f3b0f2862332e1382e058c0e493e2e79
Eldeston <fremfredric@gmail.com>
2022-06-04 00:17:07 +0400

    Fixed jittering spectral outline when TAA is on

--- shaders/main/composite.glsl
+++ shaders/main/composite.glsl
@@ -79,25 +79,39 @@ varying vec2 screenCoord;
     #include "/lib/rayTracing/volLight.glsl"
 
     #include "/lib/lighting/complexShadingDeferred.glsl"
+
+    float getSpectral(ivec2 iUv){
+        // Do a simple blur
+        float totalDepth = texelFetch(colortex3, iUv + 1, 0).z + texelFetch(colortex3, iUv - 1, 0).z +
+            texelFetch(colortex3, iUv + ivec2(1, -1), 0).z + texelFetch(colortex3, iUv - ivec2(1, -1), 0).z;
+
+        // Get the difference between the blurred samples and original
+        return abs(totalDepth * 0.25 - texelFetch(colortex3, iUv, 0).z);
+    }
     
     void main(){
+        // Screen texel coordinates
+        ivec2 screenTexelCoord = ivec2(gl_FragCoord.xy);
         // Declare and get positions
-        vec3 screenPos = vec3(screenCoord, texture2D(depthtex0, screenCoord).x);
+        vec3 screenPos = vec3(screenCoord, texelFetch(depthtex0, screenTexelCoord, 0).x);
         vec3 viewPos = toView(screenPos);
         vec3 eyePlayerPos = mat3(gbufferModelViewInverse) * viewPos;
         vec3 feetPlayerPos = eyePlayerPos + gbufferModelViewInverse[3].xyz;
 
         // Get scene color
-        vec3 sceneCol = texture2D(gcolor, screenCoord).rgb;
+        vec3 sceneCol = texelFetch(gcolor, screenTexelCoord, 0).rgb;
 
         #if ANTI_ALIASING >= 2
-            vec3 dither = toRandPerFrame(getRand3(gl_FragCoord.xy * 0.03125), frameTimeCounter);
+            vec3 dither = toRandPerFrame(getRand3(screenTexelCoord & 255), frameTimeCounter);
         #else
-            vec3 dither = getRand3(gl_FragCoord.xy * 0.03125);
+            vec3 dither = getRand3(screenTexelCoord & 255);
         #endif
 
+        // Declare and get materials
+        vec3 matRaw0 = texelFetch(colortex3, screenTexelCoord, 0).xyz;
+
         // If the object is a transparent render separate lighting
-        if(texture2D(depthtex1, screenCoord).x > screenPos.z){
+        if(texelFetch(depthtex1, screenTexelCoord, 0).x > screenPos.z){
             // Get sRGB light color
             #ifdef WORLD_LIGHT
                 vec3 lightCol = pow(LIGHT_COL_DATA_BLOCK, vec3(GAMMA));
@@ -107,15 +121,15 @@ varying vec2 screenCoord;
             // Get linear sky color
             vec3 skyCol = pow(SKY_COL_DATA_BLOCK, vec3(GAMMA));
 
-            // Declare and get materials
-            vec2 matRaw0 = texture2D(colortex3, screenCoord).xy;
-
             // Apply deffered shading
-            sceneCol = complexShadingDeferred(sceneCol, skyCol, lightCol, screenPos, viewPos, eyePlayerPos, texture2D(colortex1, screenCoord).rgb * 2.0 - 1.0, texture2D(colortex2, screenCoord).rgb, matRaw0.x, matRaw0.y, dither);
+            sceneCol = complexShadingDeferred(sceneCol, skyCol, lightCol, screenPos, viewPos, eyePlayerPos, texelFetch(colortex1, screenTexelCoord, 0).rgb * 2.0 - 1.0, texelFetch(colortex2, screenTexelCoord, 0).rgb, matRaw0.x, matRaw0.y, dither);
 
             // Fog and sky calculation
             sceneCol = getFogRender(eyePlayerPos, sceneCol, getSkyRender(vec3(0), skyCol, lightCol, normalize(eyePlayerPos), false), feetPlayerPos.y + cameraPosition.y, false);
         }
+
+        // Apply spectral effect
+        sceneCol += getSpectral(screenTexelCoord) * EMISSIVE_INTENSITY;
 
     /* DRAWBUFFERS:0 */
         gl_FragData[0] = vec4(sceneCol, 1); // gcolor
