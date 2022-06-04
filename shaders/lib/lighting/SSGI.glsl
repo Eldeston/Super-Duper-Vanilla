1c7f15282bbb19bda593f395aa77478efe169069
Eldeston <fremfredric@gmail.com>
2022-06-04 17:31:42 +0400

    SSGI optimizations

--- shaders/lib/lighting/SSGI.glsl
+++ shaders/lib/lighting/SSGI.glsl
@@ -1,22 +1,12 @@
-// Written by xirreal#0281 on ShaderLabs
-vec3 cosWeightedRandHemisphereDir(vec3 norm, vec2 seed){
-    // For TBN rotation
-    vec3 uu = normalize(cross(norm, vec3(0, 1, 1)));
-    vec3 vv = cross(uu, norm);
-
-    float radius = sqrt(seed.y);
-    float xOffset = radius * cos(PI2 * seed.x);
-    float yOffset = radius * sin(PI2 * seed.x);
-    float zOffset = sqrt(1.0 - seed.y);
-
-    // Return direction with TBN applied
-    // TBN * vec3(xOffset, yOffset, zOffset)
-    return normalize(vec3(xOffset * uu + yOffset * vv + zOffset * norm));
+// By Jessie#7257
+vec3 generateUnitVector(vec2 hash) {
+    hash.x *= PI2; hash.y = hash.y * 2.0 - 1.0;
+    return vec3(vec2(sin(hash.x), cos(hash.x)) * sqrt(1.0 - hash.y * hash.y), hash.y);
 }
 
 vec3 getSSGICoord(vec3 viewPos, vec3 screenPos, vec3 gBMVNorm, vec2 dither){
     // Get reflected screenpos
-	vec3 reflectedScreenPos = rayTraceScene(screenPos, viewPos, cosWeightedRandHemisphereDir(gBMVNorm, dither), dither.x, SSGI_STEPS, SSGI_BISTEPS);
+	vec3 reflectedScreenPos = rayTraceScene(screenPos, viewPos, normalize(gBMVNorm + generateUnitVector(dither)), dither.x, SSGI_STEPS, SSGI_BISTEPS);
     
     // Check if it's the sky and return nothing
     if(reflectedScreenPos.z < 0.5) return vec3(0);
