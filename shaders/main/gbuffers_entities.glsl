66440ca718c0decd1e77a9231e0913190df75eee
Eldeston <fremfredric@gmail.com>
2022-07-10 14:57:20 +0400

    Fixed lightning SSAO glitch

--- shaders/main/gbuffers_entities.glsl
+++ shaders/main/gbuffers_entities.glsl
@@ -151,6 +151,7 @@ uniform mat4 gbufferModelViewInverse;
         // Lightning fix, materials need to be specified due to glitching issues
         if(entityId == 10101){
             gl_FragData[0] = vec4(vec3(0.25, 0.5, 1) * EMISSIVE_INTENSITY, glcoloralpha); // gcolor
+            gl_FragData[1] = vec4(0, 0, 0, 1); // colortex1
             gl_FragData[3] = vec4(0, 0, 0, 1); // colortex3
             return;
         }
