b2d1db08dbbcc9b7c9be5cdc172743e84fba94be
Eldeston <fremfredric@gmail.com>
2022-07-22 15:09:54 +0400

    Fixed a critical issue caused by a mispelling...

--- shaders/lib/lighting/shdMapping.glsl
+++ shaders/lib/lighting/shdMapping.glsl
@@ -1,4 +1,4 @@
-#ifdef WORD_LIGHT
+#ifdef WORLD_LIGHT
 	const float sunPathRotation = 30.0; // Light angle [-60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0]
 
 	#ifdef SHD_ENABLE
