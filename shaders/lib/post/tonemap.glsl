6275a7167e768c1d253e89499b0c6d3d4d318ef9
Eldeston <fremfredric@gmail.com>
2022-07-23 16:14:37 +0400

    Adjusted tonemap (last one I swear)

--- shaders/lib/post/tonemap.glsl
+++ shaders/lib/post/tonemap.glsl
@@ -4,6 +4,5 @@ vec3 toneA(vec3 base){
 
 vec3 whitePreservingLumaBasedReinhardToneMapping(vec3 color){
 	float luma = sumOf(color) * 0.33333333;
-	return color * (1.0 + luma * 0.125) / (1.0 + luma);
-	// return color * ((1.0 - exp(-newCol)) / newCol);
+	return color * ((1.0 + luma * 0.0625) / (1.0 + luma));
 }
