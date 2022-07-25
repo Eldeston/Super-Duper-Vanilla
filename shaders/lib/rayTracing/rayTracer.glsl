40faef263c4ae722e5453cefdd89367f6aa627d6
Eldeston <fremfredric@gmail.com>
2022-07-25 10:51:25 +0400

    Fixed reflection issue at low angles

--- shaders/lib/rayTracing/rayTracer.glsl
+++ shaders/lib/rayTracing/rayTracer.glsl
@@ -1,10 +1,10 @@
-vec3 binarySearch(vec3 screenPosRayDir, vec3 startPos, int binarySearchSteps){
+vec2 binarySearch(vec3 screenPosRayDir, vec3 startPos, int binarySearchSteps){
 	for(int i = 0; i < binarySearchSteps; i++){
 		screenPosRayDir *= 0.5;
 		startPos += texture2D(depthtex0, startPos.xy).x < startPos.z ? -screenPosRayDir : screenPosRayDir;
 	}
 
-	return startPos;
+	return startPos.xy;
 }
 
 // This raytracer is so fast I swear...
@@ -16,6 +16,9 @@ vec3 rayTraceScene(vec3 screenPos, vec3 viewPos, vec3 rayDir, float dither, int 
 		vec2 handScreenPos = toScreen(viewPos + rayDir * 128.0).xy;
 		return vec3(handScreenPos, texture2D(depthtex0, handScreenPos).x != 1);
 	}
+
+	// Fix for the blob when player is near a surface. From Bálint#1673
+	if(rayDir.z > 0 && rayDir.z >= -viewPos.z) return vec3(0);
 
 	// Get screenspace rayDir
 	vec3 screenPosRayDir = normalize(toScreen(viewPos + rayDir) - screenPos) / steps;
@@ -32,7 +35,7 @@ vec3 rayTraceScene(vec3 screenPos, vec3 viewPos, vec3 rayDir, float dither, int 
 		// Check intersection
 		if(screenPos.z > currDepth && currDepth > 0.56){
 			if(binarySearchSteps == 0) return vec3(screenPos.xy, currDepth != 1);
-			return vec3(binarySearch(screenPosRayDir, screenPos, binarySearchSteps).xy, currDepth != 1);
+			return vec3(binarySearch(screenPosRayDir, screenPos, binarySearchSteps), currDepth != 1);
 		}
 	}
 	
