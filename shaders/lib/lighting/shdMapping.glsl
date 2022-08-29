b1e0d15826099273bbad24391ac86ccce4f99a75
Eldeston <fremfredric@gmail.com>
2022-08-29 23:05:36 +0400

    Moved sunPathRotation to settings.glsl

--- shaders/lib/lighting/shdMapping.glsl
+++ shaders/lib/lighting/shdMapping.glsl
@@ -1,6 +1,4 @@
 #ifdef WORLD_LIGHT
-	const float sunPathRotation = 30.0; // Light angle [-60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0]
-
 	#ifdef SHD_ENABLE
 		// Enable filtering on shadows
 		const bool shadowHardwareFiltering = true;
