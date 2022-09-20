188f810dddf9e586bcb26c6d4c7aa9d354c616ad
Staven Geoff Kho <37263820+steb-git@users.noreply.github.com>
2022-09-21 00:05:32 +0800

    Delete shaders directory

--- shaders/lib/post/tonemap.glsl
+++
@@ -1,8 +0,0 @@
-vec3 toneA(vec3 base){
-	return toneContrast(toneSaturation(base.rgb, SATURATION), CONTRAST);
-}
-
-vec3 whitePreservingLumaBasedReinhardToneMapping(vec3 color){
-	float luma = sumOf(color) * 0.33333333;
-	return color * ((1.0 + luma * 0.0625) / (1.0 + luma));
-}
--- shaders/lib/utility/convertScreenSpace.glsl
+++
@@ -1,8 +0,0 @@
-vec3 toScreen(vec3 pos){
-	vec3 data = vec3(gbufferProjection[0].x, gbufferProjection[1].y, gbufferProjection[2].z) * pos + gbufferProjection[3].xyz;
-	return (data.xyz / -pos.z) * 0.5 + 0.5;
-}
-
-float toScreen(float depth){
-	return ((gbufferProjection[2].z * depth + gbufferProjection[3].z) / -depth) * 0.5 + 0.5;
-}
