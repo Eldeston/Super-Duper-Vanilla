dedd25e429cb952cc251987dd0a6663f1a233e16
Eldeston <fremfredric@gmail.com>
2026-07-29 17:31:49 +0400

    Adjusted shadow distortion

--- shaders/lib/lighting/shdDistort.glsl
+++ shaders/lib/lighting/shdDistort.glsl
@@ -1,7 +1,11 @@
 // Shadow distortion shape
 float getDistortShape(in vec2 shdPos){
+    // Should probably swap the shape or calculations
+    // to reduce artifacts caused by the distortion
+
     // return length(shdPos);
     // return sqrt(sqrt(sumOf(squared(squared(shdPos)))));
+
     vec2 absSquare = abs(shdPos.xy);
     return smoothMax(absSquare.x, absSquare.y, 16.0);
 }
@@ -9,7 +13,7 @@ float getDistortShape(in vec2 shdPos){
 // Clip space range [-1, 1]
 vec3 getShdClipDistort(in vec3 shdPos, in float distortFactor){
     // This formula maximizes the distortion effectiveness
-    distortFactor = distortFactor + 0.1;
+    distortFactor = distortFactor * 0.9 + 0.1;
     return vec3(shdPos.xy / distortFactor, shdPos.z * 0.2);
 }
 
@@ -21,7 +25,7 @@ vec3 getShdClipDistort(in vec3 shdPos){
 // Screen space range [0, 1]
 vec3 getShdDistort(in vec3 shdPos, in float distortFactor){
     // This formula maximizes the distortion effectiveness
-    distortFactor = distortFactor * 2.0 + 0.2;
+    distortFactor = distortFactor * 1.8 + 0.2;
     return vec3(shdPos.xy / distortFactor, shdPos.z * 0.1) + 0.5;
 }
 
